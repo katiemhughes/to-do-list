73d51c029899cb91b3dc3124e25bfe44cc109951
katiehughes <katie,m.hughes@hotmail.co.uk>
2020-07-29 00:15:00 +0100

    started new attempt

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    tasks: ["Task 1", "Task 2", "Task 3"]
+  };
+
+  render() {
+    return(
+      <div className="container">
+        <div className="card frame">
+          <Header />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
+
+
