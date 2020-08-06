ae972849f441b86e6c8481bd5120feb21478275b
katiehughes <katie,m.hughes@hotmail.co.uk>
2020-08-06 23:07:05 +0100

    final CSS changes and deleteItem function fixed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React, { Component } from 'react'
-import './App.css'
-import ToDoList from "./components/ToDoList"
-import ToDoItems from "./components/ToDoItems"
+import React, { Component } from "react";
+import "./App.css";
+import ToDoList from "./components/ToDoList";
+import ToDoItems from "./components/ToDoItems";
 
 //Read up more on arrays, objects (and how to manipulate them). Also learn how to write functions and add items into an object and add items into an array. For the task list, I need an array of objects. Learn how to put them in and out of arrays.
 
 //Recreate this app.
 
-class App extends Component {      
-  constructor(props) {            
+class App extends Component {
+  constructor(props) {
     super(props);
     this.state = {
       items: [],
@@ -17,34 +17,40 @@ class App extends Component {
 
   //we don't need to define inputText in the constructor here because we aren't using it in the app component.
 
-  addItem=(task)=>{
+  addItem = (task) => {
     //using object destructuring here to avoid writing this.state.items various times below.
     const { items } = this.state;
     const newArr = [...items, task];
-    //this has to be the same as the parameter passed through.
-    this.setState({items: newArr});
-  }
 
-  //deleteItem(id)
+    //this has to be the same as the parameter passed through.
+    this.setState({ items: newArr });
+  };
+
+  deleteItem = (id) => {
+    console.log(id);
+    let items = this.state.items.filter((item, index) => {
+      return index !== id;
+    });
+    this.setState({ items });
+  };
 
   render() {
-
     const { items } = this.state;
     //this is so that I don't need to write this.state.items below (object destructuring)
-  
+
     return (
       <div className="App">
-        <h1>
-          To Do List
-        </h1>
-        <ToDoList addItem={this.addItem}/> 
-        <ToDoItems items={items}/>
+        <h1>To Do List</h1>
+        <div className="todoListWrapper">
+          <ToDoList addItem={this.addItem} />
+        </div>
+        <div className="itemsWrapper">
+          <ToDoItems items={items} deleteItem={this.deleteItem} />
+        </div>
       </div>
-    )
+    );
     //in the tags I am passing the props down to the child components (ToDoItems)
   }
 }
 
-export default App
-
-
+export default App;
--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,43 +1,40 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class ToDoList extends Component {
-    state = {                   
-      inputText:"",
-    };
-    //I don't need to define items in the state here because we don't use it in this component.
+  state = {
+    inputText: "",
+  };
+  //I don't need to define items in the state here because we don't use it in this component.
 
   handleSubmit = (e) => {
-    e.preventDefault()
-    this.props.addItem(this.state.inputText)
-    //here we are defining (with this.props.) and calling the addItem function which I created in the App.js parent component. 
-
+    e.preventDefault();
+    this.props.addItem(this.state.inputText);
+    this.setState({inputText: ""})
+    //here we are defining (with this.props.) and calling the addItem function which I created in the App.js parent component.
   };
 
   handleChange = (e) => {
-
-    this.setState({[e.target.name]: e.target.value})
-  }
-
+    this.setState({ [e.target.name]: e.target.value });
+  };
 
   render() {
-    const { inputText } = this.state
+    const { inputText } = this.state;
 
     return (
-      <div className="todoListMain">
-        <div>
+      <div className="formWrapper">
           <form onSubmit={this.handleSubmit}>
             <input
-            name="inputText"
-            value={inputText}
-            placeholder="Next task"
-            onChange={this.handleChange}
-             />
-            <button type="submit"> Add </button>
+              name="inputText"
+              value={inputText}
+              placeholder="Next task"
+              onChange={this.handleChange}
+              autocomplete="off"
+            />
+            <button className="addButton" type="submit"> Add </button>
           </form>
-        </div>
       </div>
-    )
+    );
   }
 }
 
-export default ToDoList
+export default ToDoList;
