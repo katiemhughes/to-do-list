80bc535754313b98bd97b028ff8025a514798f7d
katiehughes <katie,m.hughes@hotmail.co.uk>
2020-08-06 23:45:34 +0100

    Updates

---
+++ precache-manifest.02cd894ee44f97f2e792ab50ea455b1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3a572d5661c9716367908e1fac4b7c0",
+    "url": "/to-do-list/index.html"
+  },
+  {
+    "revision": "bdafc777c45420b9f21f",
+    "url": "/to-do-list/static/css/main.64f52909.chunk.css"
+  },
+  {
+    "revision": "848de9a77292e6c794b2",
+    "url": "/to-do-list/static/js/2.afcab787.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/to-do-list/static/js/2.afcab787.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdafc777c45420b9f21f",
+    "url": "/to-do-list/static/js/main.b9bfa88e.chunk.js"
+  },
+  {
+    "revision": "99620f4632993b275b31",
+    "url": "/to-do-list/static/js/runtime-main.65d94b1a.js"
+  }
+]);
