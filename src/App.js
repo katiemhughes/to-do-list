6ca4152578a1d90ffaba12fd805c13484ebeb866
Katie Hughes <katie.m.hughes@hotmail.co.uk>
2020-12-15 23:43:30 +0000

    Made everything responsive

--- src/App.js
+++ src/App.js
@@ -40,12 +40,14 @@ class App extends Component {
 
     return (
       <div className="App">
-        <h1>To Do List</h1>
-        <div className="todoListWrapper">
-          <ToDoList addItem={this.addItem} />
-        </div>
-        <div className="itemsWrapper">
-          <ToDoItems items={items} deleteItem={this.deleteItem} />
+        <div className="app-container">
+          <h1 className="app-title">To Do List</h1>
+          <div className="todoListWrapper">
+            <ToDoList addItem={this.addItem} />
+          </div>
+          <div className="itemsWrapper">
+            <ToDoItems items={items} deleteItem={this.deleteItem} />
+          </div>
         </div>
       </div>
     );
